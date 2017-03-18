e92bdf869e6409345a3a3e2602c585589a42451a
mattiaslj <ml223dq@student.lnu.se>
2017-03-18 12:12:52 +0100

    added load more button, taking into account if user searched or not. Also added load recent button. Some cleanup

--- JavaScript/flickrService.js
+++ JavaScript/flickrService.js
@@ -3,12 +3,32 @@
 
 		var key = "92217f00479cdc382a7c4de5ef0a8ec1";
 		var secret = "98e6e56a3b566d95";
+		var isSearch = false;
+		var page = 0;
 
 		var flickrService = function (method, tags) {
+
 			return new Promise(resolve => {
-				var url = 'https://api.flickr.com/services/rest/?method=' +
-					method + '&api key=' + key + '&tags=' + tags +
-					'&safe_search=1&per_page=25&format=json&nojsoncallback=1';
+				var url;
+				// empty search gives recent photos
+				if (tags == null || tags.trim() === '') {
+					// reset pageCount
+					page = 0;
+					isSearch = false;
+
+					method = "flickr.photos.getRecent";
+					url = 'https://api.flickr.com/services/rest/?method=' +
+						method + '&api key=' + key + '&tags=' +
+						'&safe_search=1&per_page=25&format=json&nojsoncallback=1';
+				} else {
+					// add to pagecount to not get same images
+					page += 1;
+					isSearch = true;
+					url = 'https://api.flickr.com/services/rest/?method=' +
+						method + '&api key=' + key + '&tags=' + tags +
+						'&safe_search=1&page=' + page +
+						'&per_page=25&format=json&nojsoncallback=1';
+				}
 
 				var req = new XMLHttpRequest();
 				req.open('GET', url);
@@ -29,8 +49,14 @@
 				req.send();
 			});
 		}
+
+		var userSearched = function () {
+			return isSearch;
+		}
+
 		return {
-			flickrService: flickrService
+			flickrService: flickrService,
+			userSearched: userSearched
 		};
 	}());
 })();
--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,59 +3,105 @@
     'use strict';
     (function () {
         bildgalleri.main = (function () {
-            // search flickr for images by tags
+
+            /*
+                Load images when first entering page
+            */
+            loadRecentImages();
+
+            /*
+                EventListener for Searchbutton
+                search flickr for images by tags
+            */
             document.getElementById("searchButton").addEventListener('click', function () {
-                var method = "flickr.photos.search";
-                var tags = document.getElementById("searchField").value;
+                // remove old images if its a first search
+                if (!bildgalleri.flickrService.userSearched())
+                    document.getElementById("mainContent").innerHTML = "";
 
-                bildgalleri.flickrService.flickrService(method, tags).then(function (response) {
-                    console.log(response);
-                    var result = JSON.parse(response);
-                    var result = result.photos.photo;
-                    appendImages(result);
-                }, function (error) {
+                userSearch();
+            });
 
-                });
+            /*
+               EventListener for Load More Images Button
+            */
+            document.getElementById("loadImages").addEventListener('click', function () {
+                // if user searched it wants to see more images related to search
+                // else user gets to see random new pictures
+                if (bildgalleri.flickrService.userSearched()) {
+                    userSearch();
+                } else {
+                    loadRecentImages();
+                }
+
+            });
+
+            /*
+                EventListener for Get recent Button
+            */
+            document.getElementById("getRecentButton").addEventListener('click', function () {
+                document.getElementById("mainContent").innerHTML = "";
+                loadRecentImages();
             });
 
             /*
                 uses flickrService.getRecent on entering homepage
             */
-            function setup() {
+            function loadRecentImages() {
                 var method = "flickr.photos.getRecent";
                 bildgalleri.flickrService.flickrService(method, "").then(function (response) {
                     var result = JSON.parse(response);
                     var result = result.photos.photo;
                     appendImages(result);
 
-                }, function (error) { });
+                }, function (error) {
+                    console.log("something went wrong")
+                });
             }
-            setup();
+
+            function userSearch() {
+                var method = "flickr.photos.search";
+                var tags = document.getElementById("searchField").value;
+                // Get images from flickr
+                bildgalleri.flickrService.flickrService(method, tags).then(function (response) {
+                    var result = JSON.parse(response);
+                    var result = result.photos.photo;
+
+                    // append images to mainContent
+                    appendImages(result);
+                }, function (error) {
+                    console.log("something went wrong");
+                });
+            }
+
+            /*
+                ####### Help methods ###########
+            */
+
+            var appendImages = function (jsonObj) {
+                for (var key in jsonObj) {
+                    var photoUrl = "http://farm" + jsonObj[key].farm + ".static.flickr.com/"
+                        + jsonObj[key].server + "/" + jsonObj[key].id
+                        + "_" + jsonObj[key].secret + "_" + "b.jpg";
+                    /*
+                      var a_href = "http://www.flickr.com/photos/" +
+                      jsonObj[key].owner + "/" + jsonObj[key].id + "/";
+                    
+                      var link = document.createElement("a");
+                      link.href = a_href;
+                      link.innerHTML = key;*/
+
+                    var img = document.createElement("img");
+                    img.src = photoUrl;
+                    img.style.height = '300px';
+                    img.style.width = '300px';
+                    var src = document.getElementById("mainContent");
+                    src.appendChild(img);
+                    // src.appendChild(link);
+                }
+            }
+
+
+
         }());
-
-
-        var appendImages = function (jsonObj) {
-            for (var key in jsonObj) {
-                console.log()
-                var photoUrl = "http://farm" + jsonObj[key].farm + ".static.flickr.com/"
-                    + jsonObj[key].server + "/" + jsonObj[key].id
-                    + "_" + jsonObj[key].secret + "_" + "b.jpg";
-
-                var a_href = "http://www.flickr.com/photos/" +
-                    jsonObj[key].owner + "/" + jsonObj[key].id + "/";
-
-                var link = document.createElement("a");
-                link.href = a_href;
-                link.innerHTML = key;
-
-                var img = document.createElement("img");
-                img.src = photoUrl;
-                img.style.height = '300px';
-                img.style.width = '300px';
-                var src = document.getElementById("mainContent");
-                src.appendChild(img);
-                src.appendChild(link);
-            }
-        }
     }());
 }());
