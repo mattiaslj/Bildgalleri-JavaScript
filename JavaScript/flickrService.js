2f20769e677483dcafb78487ebac6724b4aa873e
mattiaslj <ml223dq@student.lnu.se>
2017-03-19 09:55:13 +0100

    added clickable images, changed structure in flickrService, added some styling

--- JavaScript/flickrService.js
+++ JavaScript/flickrService.js
@@ -6,30 +6,8 @@
 		var isSearch = false;
 		var page = 0;
 
-		var flickrService = function (method, tags) {
-
+		var service = function (url) {
 			return new Promise(resolve => {
-				var url;
-				// empty search gives recent photos
-				if (tags == null || tags.trim() === '') {
-					// reset pageCount
-					page = 0;
-					isSearch = false;
-
-					method = "flickr.photos.getRecent";
-					url = 'https://api.flickr.com/services/rest/?method=' +
-						method + '&api key=' + key + '&tags=' +
-						'&safe_search=1&per_page=25&format=json&nojsoncallback=1';
-				} else {
-					// add to pagecount to not get same images
-					page += 1;
-					isSearch = true;
-					url = 'https://api.flickr.com/services/rest/?method=' +
-						method + '&api key=' + key + '&tags=' + tags +
-						'&safe_search=1&page=' + page +
-						'&per_page=25&format=json&nojsoncallback=1';
-				}
-
 				var req = new XMLHttpRequest();
 				req.open('GET', url);
 
@@ -50,12 +28,48 @@
 			});
 		}
 
+		var search = function (tags) {
+			var url;
+			var method;
+			// empty search gives recent photos
+			if (tags == null || tags.trim() === '') {
+				// reset pageCount
+				page = 0;
+				isSearch = false;
+
+				method = "flickr.photos.getRecent";
+				url = 'https://api.flickr.com/services/rest/?method=' +
+					method + '&api key=' + key + '&tags=' +
+					'&safe_search=1&per_page=24&format=json&nojsoncallback=1';
+			} else {
+				// add to pagecount to not get same images
+				method = 'flickr.photos.search';
+				page += 1;
+				isSearch = true;
+				url = 'https://api.flickr.com/services/rest/?method=' + method +
+					'&api key=' + key + '&tags=' + tags +
+					'&safe_search=1&page=' + page +
+					'&per_page=24&format=json&nojsoncallback=1';
+			}
+			return service(url);
+		}
+
+		var getRecent = function () {
+			var url;
+			var method = 'flickr.photos.getRecent';
+			url = 'https://api.flickr.com/services/rest/?method=' +
+				method + '&api key=' + key + '&per_page=24&format=json&nojsoncallback=1';
+
+			return service(url);
+		}
+
 		var userSearched = function () {
 			return isSearch;
 		}
 
 		return {
-			flickrService: flickrService,
+			getRecent: getRecent,
+			search: search,
 			userSearched: userSearched
 		};
 	}());
--- JavaScript/main.js
+++ JavaScript/main.js
@@ -16,7 +16,7 @@
             document.getElementById("searchButton").addEventListener('click', function () {
                 // remove old images if its a first search
                 if (!bildgalleri.flickrService.userSearched())
-                    document.getElementById("mainContent").innerHTML = "";
+                    document.getElementById("photos").innerHTML = "";
 
                 userSearch();
             });
@@ -39,8 +39,13 @@
                 EventListener for Get recent Button
             */
             document.getElementById("getRecentButton").addEventListener('click', function () {
-                document.getElementById("mainContent").innerHTML = "";
+                document.getElementById("photos").innerHTML = "";
                 loadRecentImages();
+            });
+
+            document.getElementById("back").addEventListener("click", function () {
+                document.getElementById("originalPhoto").style.display = 'none';
+                document.getElementById("photos").style.display = 'inline';
             });
 
             /*
@@ -48,7 +53,7 @@
             */
             function loadRecentImages() {
                 var method = "flickr.photos.getRecent";
-                bildgalleri.flickrService.flickrService(method, "").then(function (response) {
+                bildgalleri.flickrService.getRecent().then(function (response) {
                     var result = JSON.parse(response);
                     var result = result.photos.photo;
                     appendImages(result);
@@ -62,7 +67,7 @@
                 var method = "flickr.photos.search";
                 var tags = document.getElementById("searchField").value;
                 // Get images from flickr
-                bildgalleri.flickrService.flickrService(method, tags).then(function (response) {
+                bildgalleri.flickrService.search(tags).then(function (response) {
                     var result = JSON.parse(response);
                     var result = result.photos.photo;
 
@@ -73,35 +78,59 @@
                 });
             }
 
-            /*
-                ####### Help methods ###########
-            */
-
+            // creates image objects and attaches eventlistener.
+            // appens images to dom
             var appendImages = function (jsonObj) {
+                document.getElementById("photos").style.display = 'inline';
+                document.getElementById("originalPhoto").style.display = 'none';
                 for (var key in jsonObj) {
-                    var photoUrl = "http://farm" + jsonObj[key].farm + ".static.flickr.com/"
-                        + jsonObj[key].server + "/" + jsonObj[key].id
-                        + "_" + jsonObj[key].secret + "_" + "b.jpg";
-                    /*
-                      var a_href = "http://www.flickr.com/photos/" +
-                      jsonObj[key].owner + "/" + jsonObj[key].id + "/";
-                    
-                      var link = document.createElement("a");
-                      link.href = a_href;
-                      link.innerHTML = key;*/
+                    // closure to make eventlisteners stick to its object, because hoisting and variables and stuff
+                    (function () {
+                        var photoUrl = "http://farm" + jsonObj[key].farm + ".static.flickr.com/"
+                            + jsonObj[key].server + "/" + jsonObj[key].id
+                            + "_" + jsonObj[key].secret + "_" + "b.jpg";
 
-                    var img = document.createElement("img");
-                    img.src = photoUrl;
-                    img.style.height = '300px';
-                    img.style.width = '300px';
-                    var src = document.getElementById("mainContent");
-                    src.appendChild(img);
-                    // src.appendChild(link);
+                        // create img
+                        var img = document.createElement("img");
+                        img.src = photoUrl;
+                        img.style.height = '300px';
+                        img.style.width = '300px';
+
+                        // append image to container, and container to document
+                        var src = document.getElementById("photos");
+                        src.appendChild(img);
+                        //  src.appendChild(link);
+
+                        img.onclick = function () {
+                            // hide all images
+                            document.getElementById("photos").style.display = 'none';
+                            var test = document.getElementById("originalPhoto");
+                            if (test.childNodes.length > 1) {
+                                test.removeChild(test.childNodes[1]);
+                            }
+                            document.getElementById("originalPhoto").style.display = 'inline';
+                            var temp = document.createElement("img");
+                            temp.src = photoUrl;
+                            temp.style.height = '800px';
+                            temp.style.width = '800px';
+                            // append image
+                            var src = document.getElementById("originalPhoto");
+                            src.appendChild(temp);
+                            //    src.appendChild(link);
+                        };
+                    }());
                 }
             }
-
-
-
         }());
     }());
 }());
+
+/*
+                    var a_href = "http://www.flickr.com/photos/" +
+                        jsonObj[key].owner + "/" + jsonObj[key].id + "/";
+
+                    var link = document.createElement("a");
+                    link.href = a_href;
+                    link.innerHTML = key;
+*/
+
